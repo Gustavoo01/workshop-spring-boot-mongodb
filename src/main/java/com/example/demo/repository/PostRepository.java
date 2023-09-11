cc2136f186df58b0792724c0f432ef9a1b3fb7ce
Gustavoo01 <gustavo.de.souza@hotmail.com>
2023-09-11 16:55:58 -0300

    Consulta simples com query methods

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.Post;
+import java.util.List;
+
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-    
+    List<Post> findByTitleContainingIgnoreCase(String title);
 }
